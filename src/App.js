0eeac798cc3b0356cf57ce40a606da050fccc95c
yvkam04 <yvkam04@gmail.com>
2026-07-01 13:09:52 +0200

    Fix import PrivateRoute

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
 import Companies from "./pages/Companies";
 import Users from "./pages/Users";
-import PrivateRoute from "./PrivateRoute";
+import PrivateRoute from "./pages/PrivateRoute";
 
 function App() {
   return (
